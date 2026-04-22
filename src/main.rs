bc79e05532b22bdde07df5084a78de0b2f153a20
Dai Duong Le <daiduonglebusiness@gmail.com>
2026-04-22 19:02:00 +0200

    Updated main to include correct params

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ fn main() {
 
     let material_ground = Rc::new(Lambertian::new(&Vec3(0.8, 0.8, 0.0)));
     let material_center = Rc::new(Lambertian::new(&Vec3(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Metal::new(&Vec3(0.8, 0.8, 0.8)));
-    let material_right = Rc::new(Metal::new(&Vec3(0.8, 0.6, 0.2)));
+    let material_left = Rc::new(Metal::new(&Vec3(0.8, 0.8, 0.8), 0.0));
+    let material_right = Rc::new(Metal::new(&Vec3(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
         &Vec3(0.0, -100.5, -1.0),
